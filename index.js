9b1ef121103822512b92814466483d47e7391d72
Björn Jentschke <jentschke1@gmail.com>
2024-01-24 22:45:25 +0100

    add inteactive answer button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // phew… not a lot going on here. Please add some code!
+
+// 1. add toggle function to bookmark
 console.clear();
 
 const buttonBookmark = document.querySelector('[data-js="bookmarkToggle"]')
@@ -6,6 +8,20 @@ const buttonBookmark = document.querySelector('[data-js="bookmarkToggle"]')
 console.log(buttonBookmark);
 
 buttonBookmark.addEventListener("click", () => {
-    buttonBookmark.classList.toggle("bookmark--switchactive")
+    buttonBookmark.classList.toggle("bookmark--active")
+})
+
+// 2. add hidden function to answer box
+const answer = document.querySelector('[data-js="showAnswer"]');
+const answerText = document.querySelector('[data-js="textAnswer"]')
+
+answer.addEventListener("click", () => {
+    answerText.classList.toggle("card__answer--active");
+    
+    if (answerText.classList.contains("card__answer--active")) {
+        answer.textContent = "Hide answer";
+    } else {
+        answer.textContent = "Show answer";
+    }
 })
 
