5c391033a202dad2ffa39e58b4ee57c3f5c7780c
Björn Jentschke <jentschke1@gmail.com>
2024-01-25 10:13:27 +0100

    show and hide answer

--- index.js
+++ index.js
@@ -12,16 +12,48 @@ buttonBookmark.addEventListener("click", () => {
 })
 
 // 2. add hidden function to answer box
-// const answer = document.querySelector('[data-js="showAnswer"]');
-// const answerText = document.querySelector('[data-js="textAnswer"]')
 
-// answer.addEventListener("click", () => {
-//     answerText.classList.toggle("card__answer--active");
+// answerbox1:
+const answerButton = document.querySelector('[data-js="showAnswer"]');
+const answerText = document.querySelector('[data-js="textAnswer"]');
+
+answerButton.addEventListener("click", () => {
+    answerText.classList.toggle("card__answer--active");
     
-//     if (answerText.classList.contains("card__answer--active")) {
-//         answer.textContent = "Hide answer";
-//     } else {
-//         answer.textContent = "Show answer";
-//     }
-// })
+    if (answerText.classList.contains("card__answer--active")) {
+        answerButton.textContent = "Hide answer";
+    } else {
+        answerButton.textContent = "Show answer";
+    }
+})
+
+// answerbox2
+
+const answerButtonTwo = document.querySelector('[data-js="showAnswerTwo"]');
+const answerTextTwo = document.querySelector('[data-js="textAnswerTwo"]');
+
+answerButtonTwo.addEventListener("click", () => {
+    answerTextTwo.classList.toggle("card__answer--active");
+
+   if (answerTextTwo.classList.contains("card__answer--active")) {
+        answerButtonTwo.textContent = "Hide answer";
+    } else {
+        answerButtonTwo.textContent = "Show answer";
+    }
+});
+
+// answerbox3
+
+const answerButtonThree = document.querySelector('[data-js="showAnswerThree"]');
+const answerTextThree = document.querySelector('[data-js="textAnswerThree"]');
+
+answerButtonThree.addEventListener("click", () => {
+    answerTextThree.classList.toggle("card__answer--active");
+
+    if (answerTextThree.classList.contains("card__answer--active")) {
+        answerButtonThree.textContent = "Hide answer";
+    } else {
+        answerButtonThree.textContent = "Show answer";
+    }
+});
 
